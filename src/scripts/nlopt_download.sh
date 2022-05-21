ae83e53ca703158afeddf832b55f7a44e8977e36
Matthew Wheeler <wheelemw@gmail.com>
2022-05-21 10:31:52 -0400

    CRAN ready.

--- src/scripts/nlopt_download.sh
+++ src/scripts/nlopt_download.sh
@@ -1,15 +1,6 @@
 #! /bin/sh
 
 RSCRIPT_BIN=$1
-echo $1
-echo $1
-echo $1
-echo $1
-echo $1
-echo "-----------------------------"
-echo "-----------------------------"
-echo "-----------------------------"
-echo "-----------------------------"
 
 # Uncompress NLOPT source
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'nlopt-src.tar.gz')"
