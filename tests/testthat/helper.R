4a68516ef1ffac3be586e93cfcd55ad3f0dce8e7
Matthew Wheeler <wheelemw@gmail.com>
2022-06-01 10:03:50 -0400

    Update helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,5 +1,26 @@
 library(actuar)
 
+build_ma_dataset_2 <- function(){
+  Hill.p <- rbind(c(481,-250.3,70,3.3),
+                  c(481,-250.3,40,1.3),
+                  c(481,-250.2,15,1.1),
+                  c(481,-250.3,50,4) ,
+                  c(10.58,9.7,70,3.5),
+                  c(10.58,9.7,25,3),
+                  c(10.58,9.7,15,2),
+                  c(10.58,9.7,50,4))
+  hill <- data.frame(a=Hill.p[,1],b=Hill.p[,2],
+                     c=Hill.p[,3],d=Hill.p[,4])
+  
+  
+  doses <- rep(c(0,6.25,12.5,25,50,80,100,150),each=10)
+  
+  mean <- ToxicR:::.cont_hill_f(as.numeric(hill[1,]),doses)
+  set.seed(2020)
+  y <- rinvgauss(length(mean),mean,45528.14)
+  return(list(doses=doses, y=y))
+}
+
 build_single_continuous_dataset <- function(){
      M2           <- matrix(0,nrow=5,ncol=4)
      colnames(M2) <- c("Dose","Resp","N","StDev")
